2c06e67e270fbd83ec274d5f4e0e89514d6d6d55
salonivishwakarma454-ux <salonivishwakarma454@gmail.com>
2025-12-29 10:50:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,23 +10,23 @@ st.title('🏠House Price prediction using ML')
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
 
 df = pd.read_csv('house_data.csv')
-x = df.iloc[:,:-3]
+X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
-final_x = x
+final_X = X
 scaler = StandardScaler()
-scaled_x = scaler.fit_transform(final_x)
+scaled_X = scaler.fit_transform(final_X)
 
-st.sidebar.title('Select House Features: ')
+st.sidebar.title('Select House features: ')
 st.sidebar.image('https://cdn.dribbble.com/userupload/20000742/file/original-aaf23458355a156d0cf85b8217a5065a.gif')
 all_value = []
-for i in final_x:
+for i in final_X:
   min_value = final_X[i].min()
   max_value = final_X[i].max()
-  result = st.sidebar.slider(f'Select {i} value',min_value,max_value)
+  result = st.sidebar.slider(f'select {i} value', min_value, max_value)
   all_value.append(result)
 
-user_X = scaler.transform([all_value])
+user_X = scaler.transform = ([all_value])
 
 @st.cache_data
 def ml_model(X,y):
@@ -34,16 +34,18 @@ def ml_model(X,y):
   model.fit(X,y)
   return model
 
-model = ml_model(model_X,y)
-house_price = model.predict(user_X)[0]
-final_price = round(house_price * 100000,2)
+model = ml_model(scaled_X ,y)
 
-with st.spinner('Prediction House Price'):
+house_price = model.predict(user_X)[0]
+
+final_price = round(house_price * 100000,2)
+with st.spinner('Predicting House Price'):
   import time
   time.sleep(2)
 
-st.success(f'Estimated House Price is: $ {final_price}')
-st.markdown('''**Design and Developed by Vinayak Vishwakarma**''')
+st.success(f'Estimated House price is: $ {final_price}')
+st.markdown('''**Design and Development by: Vinayak Vishwakarma**''')
+
 
 
 
