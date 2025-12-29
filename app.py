eeb5d3001eebc60d198022c1712ecd829380e55e
salonivishwakarma454-ux <salonivishwakarma454@gmail.com>
2025-12-29 10:19:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,19 @@ st.title('🏠House Price prediction using ML')
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
 
 df = pd.read_csv('house_data.csv')
-X = df.iloc[:,:-3]
+x = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
-final_X = X
+final_x = x
 scaler = StandardScaler()
-scaled_X = scaler.fit_transform(final_X)
+scaled_x = scaler.fit_transform(final_x)
 
-st.sidebar.title('Select House Feature')
+st.sidebar.title('Select House Features: ')
+st.sidebar.image('https://cdn.dribbble.com/userupload/20000742/file/original-aaf23458355a156d0cf85b8217a5065a.gif')
+all_value = []
+for i in final_x:
+  result = st.sidebar.slider(f'Select {i} value')
+  all_value.append(result)
+
+st.write(all_value)
+
